bee79ca8d8b0e7d2b9f2af1d93503c384818dd4a
Lotrando <miroslav.klika@seznam.cz>
2024-09-03 22:48:37 +0200

    Fix css and font in appblade.php and Welcome.vue component

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,7 +1,9 @@
 <script setup>
 </script>
 <template>
-  <div class="h-screen flex items-center justify-center">
-    <h1>Welcome</h1>
+  <div
+    class="h-screen flex items-center justify-center bg-gradient-to-tr from-purple-600 to-sky-600"
+  >
+    <h1 class="font-inter text-white">Welcome</h1>
   </div>
 </template>
