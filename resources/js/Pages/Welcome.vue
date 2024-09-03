5fb2b94ccefd2f983c15fae6ab90c282f55081cc
Lotrando <miroslav.klika@seznam.cz>
2024-09-03 22:36:43 +0200

    Install inertia monolith

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+  <div class="h-screen flex items-center justify-center">
+    <h1>Welcome</h1>
+  </div>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
-import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp, Head, Link } from '@inertiajs/vue3'
+import Layout from './Template/Layout.vue'
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        let page = pages[`./Pages/${name}.vue`]
+        page.default.layout = page.default.layout || Layout
+        return page
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .component('Head', Head)
+            .component('Link', Link)
+            .mount(el)
+    },
+    progress: {
+        delay: 250,
+        color: '#58f',
+        includeCSS: true,
+        showSpinner: false,
+    },
+})
